dfbc6e85e867ba7d0131f6b3c94e59a12054429e
nino059 <captain.ninoo@gmail.com>
2025-02-09 23:04:45 +0700

    demo_test

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+            int a = int.Parse(Console.ReadLine());
+            int c = a, b = 0;
+            for (int i = 0; a > 0; i += 1)
+            {
+                //Console.WriteLine("i = " + i);
+                //Console.WriteLine("b = " + b);
+                //Console.WriteLine("a = " + a);
+                //Console.WriteLine("10 * b = " + (10 * b));
+                //Console.WriteLine("a%10 = " + a%10);
+                b = (int)((10 * b) + a % 10);
+                a /= 10;
+            }
+            Console.WriteLine(b);
+            Console.WriteLine((c==b)?"True":"False");
+            return 0;
+        }
+    }
+}
